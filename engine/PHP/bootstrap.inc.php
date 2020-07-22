50d03cd1ab7b1aa6409344338d072d6327be4b3a
Evan Wills <evan.wills@acu.edu.au>
2020-07-23 09:44:34 +1000

    added bootstrap file for debugging.

---
+++ engine/PHP/bootstrap.inc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file contains basic code to bootstrap debugging the API
+ *
+ * PHP version 7.4
+ *
+ * @category RegexAPI
+ * @package  RegexAPI
+ * @author   Evan Wills <evan.i.wills@gmail.com>
+ * @license  MIT <url>
+ * @link     https://github.com/regex-api
+ */
+
+
+if (!function_exists('debug')) {
+    function debug()
+    {
+    }
+}
